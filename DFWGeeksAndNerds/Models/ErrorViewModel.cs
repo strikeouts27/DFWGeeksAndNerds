e6e8aea5e67919ebb0b2b11512241903056cb605
strikeouts27 <99090208+strikeouts27@users.noreply.github.com>
2026-03-02 20:01:19 -0600

    inital commit for project setup

---
+++ DFWGeeksAndNerds/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DFWGeeksAndNerds.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
