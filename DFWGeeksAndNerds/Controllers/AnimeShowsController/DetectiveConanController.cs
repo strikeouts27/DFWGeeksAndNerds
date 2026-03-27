622cc9ee679bc2bf294f5668d2a9221576581a79
strikeouts27 <99090208+strikeouts27@users.noreply.github.com>
2026-03-27 17:00:39 -0500

    establishing article page for anime shows

---
+++ DFWGeeksAndNerds/Controllers/AnimeShowsController/DetectiveConanController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DFWGeeksAndNerds.Controllers.AnimeShowsController
+{
+    public class DetectiveConanController : Controller
+    {
+        public IActionResult DetectiveConan()
+        {
+            return View();
+        }
+    }
+}
