54fb5519f28215a83f8041de7a4eb6a9029ee09f
strikeouts27 <99090208+strikeouts27@users.noreply.github.com>
2026-05-28 16:42:23 -0500

    05.28.26 start of session for understanding delete methods

--- DFWGeeksAndNerds/Controllers/EventsController.cs
+++ DFWGeeksAndNerds/Controllers/EventsController.cs
@@ -62,8 +62,10 @@ namespace DFWGeeksAndNerds.Controllers
                 // 2. Redirect to a "Success" or "Index" page
                 // conversion 
                 var eventDTO = EventViewModel.ConvertToEventDTO(model);
+                // at the end of this method call the post request has returned with a status code of success or thrown an error. 
                 await _eventDataService.CreateEventAsync(eventDTO);
             }
+            // this actually is a clever method to activate the get request for this page and show the new event created. 
             return RedirectToAction("Events");
         }
 
